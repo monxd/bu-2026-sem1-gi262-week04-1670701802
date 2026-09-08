a2d0a732438dfd0f41ad8332f0d591797f293ce8
monxd <rattanasakhunchonpurin@gmail.com>
2026-09-08 17:07:27 +0700

    Workshopในคาบ

--- Assets/Scripts/Workspace/Assignment/Lecture.cs
+++ Assets/Scripts/Workspace/Assignment/Lecture.cs
@@ -17,14 +17,49 @@ namespace Assignment
 
         public void LCT01_SyntaxList()
         {
-            throw new System.NotImplementedException();
+            List<string> lists = new List<string>();
         }
 
         public void LCT02_SyntaxLinkedList()
         {
-            throw new System.NotImplementedException();
+            LinkedList<string> linkedList = new LinkedList<string>();
+            linkedList.AddLast("Node 1");
+            linkedList.AddLast("Node 2");
+            linkedList.AddFirst("Node 0");
+            PrintLinkList(linkedList);
+
+            LinkedListNode<string> firstNode = linkedList.First;
+            Debug.Log("first: " + firstNode.Value);
+            LinkedListNode<string> lastNode = linkedList.Last;
+            Debug.Log("last: " +  lastNode.Value);
+            LinkedListNode<string> node1 = linkedList.Find("Node 1");
+            Debug.Log("node: " + node1.Value);
+            Debug.Log(node1.Previous.Value);
+            Debug.Log(node1.Next.Value);
+
+            if (firstNode.Previous == null) { Debug.Log("firstNode.Previous is null"); }
+            if (lastNode.Next == null) { Debug.Log("lastNode.Next is null"); }
+
+            linkedList.AddAfter(node1, "Node 1.5");
+            linkedList.AddBefore(node1, "Node 0.5");
+            PrintLinkList(linkedList);
+
+            linkedList.RemoveFirst();
+            PrintLinkList(linkedList);
+            linkedList.Remove("Node 2");
+            PrintLinkList(linkedList);
+            linkedList.Clear();
+            PrintLinkList(linkedList);
         }
 
+        void PrintLinkList(LinkedList<string> linkedList)
+        {
+            Debug.Log("-----linkedList-----");
+            foreach (string s in linkedList)
+            {
+                Debug.Log(s);
+            }
+        }
         public void LCT03_SyntaxHashTable()
         {
             throw new System.NotImplementedException();
@@ -32,7 +67,39 @@ namespace Assignment
 
         public void LCT04_SyntaxDictionary()
         {
-            throw new System.NotImplementedException();
+            Dictionary<int,string> dictionary = new Dictionary<int,string>();
+            dictionary.Add(1, "apple");
+            dictionary.Add(2, "Banana");
+            dictionary[3] = "Cherry";
+
+            //Debug.Log(dictionary[4]);
+            int keytocheck = 1;
+            bool haskey = dictionary.ContainsKey(keytocheck);
+            Debug.Log($"has key {keytocheck} : {haskey}");
+            if (haskey )
+            {
+                Debug.Log(dictionary[keytocheck]);
+            }
+
+            foreach (int  k in dictionary.Keys)
+            {
+                Debug.Log(k);
+
+            }
+            foreach (string s in dictionary.Values)
+            {
+                Debug.Log(s);
+            }
+            dictionary.Remove(1);
+            foreach (string s in dictionary.Values)
+            {
+                Debug.Log(s);
+            }
+            dictionary.Clear();
+            foreach (string s in dictionary.Values)
+            {
+                Debug.Log(s);
+            }
         }
 
         #endregion
--- Assets/Workshop/Student/Scripts/ZombieParade.cs
+++ Assets/Workshop/Student/Scripts/ZombieParade.cs
@@ -53,23 +53,40 @@ namespace Solution
         IEnumerator MoveParade()
         {
             //0. สร้างหัวงู
+            Parade.AddFirst(this.gameObject);
 
             while (isAlive)
             {
                 // 1. ดึงส่วนแรกของงูออกมา
-
+                LinkedListNode<GameObject> firstNode = Parade.First;
+                GameObject firstPart = firstNode.Value;
                 // 2. ดึงส่วนสุดท้ายของงูออกมา
-             
+                LinkedListNode<GameObject> lastNode = Parade.Last;
+                GameObject lastPart = lastNode.Value;
                 // 3. ลบส่วนสุดท้ายออกจาก LinkedList
-
+                Parade.RemoveLast();
                 // 5. กำหนดตำแหน่งและทิศทางของส่วนที่ถูกย้ายมาใหม่
                 // ให้ไปอยู่ที่ตำแหน่งของส่วนหัวงู (ซึ่งเพิ่งเคลื่อนที่ไปเมื่อครู่)
-   
-                //6. เคลื่อนที่
+                int toX = 0;
+                int toY = 0;
 
+                bool isCollide = true;
+                while (isCollide == true)
+                {
+
+
+                     moveDirection = RandomizeDirection();
+                    toX = (int)(firstPart.transform.position.x + moveDirection.x);
+                    toY = (int)(firstPart.transform.position.y + moveDirection.y);
+                    isCollide = IsCollision(toX, toY);
+                }
+                //6. เคลื่อนที่
+                mapGenerator.mapdata[positionX, positionY] = null;
+                positionX = toX;
+                positionY = toY;
                 // 7. เพิ่มส่วนนั้นกลับเข้าไปเป็นส่วนที่สองของ LinkedList
                 // (ซึ่งก็คือส่วนแรกของลำตัว)
-
+                Parade.AddFirst(lastNode);
                 // รอตามเวลาที่กำหนดก่อนการเคลื่อนที่ครั้งต่อไป
                 yield return new WaitForSeconds(moveInterval);
             }
@@ -77,7 +94,10 @@ namespace Solution
         private bool IsCollision(int x, int y)
         {
             // 4. ตรวจสอบสิ่งกีดขวาง
-            
+            if(HasPlacement(x, y))
+            {
+                return true;
+            }
             return false;
         }
         void Move(Vector2 direction,GameObject targetMove)
